da83a7976cfc3bc017b7c63895edcc0195507450
jondev <jonmac2454@gmail.com>
2025-03-05 17:20:30 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Amanda Brown"; 
+
+
+const welcomeMessage = 'Welcome to Flatbook, $(Amanda Brown)!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, $(Amanda.slice(0, 1)!';
